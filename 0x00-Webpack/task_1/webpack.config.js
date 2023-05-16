42ff0a012d7ac8186d33eb303e9d9fe513434435
seyistry <seyistry@gmail.com>
2023-05-16 11:11:21 +0100

    remove package

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -9,10 +8,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack Output'
-    })
-  ]
+  }
 };
