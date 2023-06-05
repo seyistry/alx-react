c1edebd74d0e935b8a9487ae5f732cb820efd577
seyistry <seyistry@gmail.com>
2023-06-05 18:55:20 +0100

    fix error

--- 0x05-react_state/task_0/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_0/dashboard/src/Login/Login.js
@@ -1,59 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 function Login() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [enableSubmit, setEnableSubmit] = useState(false);
-  const handleLoginSubmit = () => setIsLoggedIn(true);
-  const handleChangeEmail = (e) => {
-    e.preventDefaults();
-    setEmail(e.target.value());
-    if (email.length > 1 && password.length > 1) {
-      setEnableSubmit(true);
-    } else {
-      setEnableSubmit(false);
-    }
-  };
-  const handleChangePassword = (e) => {
-    e.preventDefaults();
-    setPassword(e.target.value());
-    if (email.length > 1 && password.length > 1) {
-      setEnableSubmit(true);
-    } else {
-      setEnableSubmit(false);
-    }
-  };
   return (
-    <form className={css(styles.large)}>
+    <div className={css(styles.large)}>
       <label className={css(styles.large)} htmlFor="email">
         Email
       </label>
-      <input
-        type="text"
-        id="email"
-        name="email"
-        onChange={() => handleChangeEmail}
-      />
+      <input type="text" id="email" name="email" />
       <label className={css(styles.large)} htmlFor="password">
         Password
       </label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        onChange={() => handleChangePassword}
-      />
-      <input
-        type="submit"
-        className={css(styles.large, styles.btn)}
-        onClick={() => handleLoginSubmit}
-        disabled={!enableSubmit}
-      >
-        ok
-      </input>
-    </form>
+      <input type="password" id="password" name="password" />
+      <button className={css(styles.large, styles.btn)}>ok</button>
+    </div>
   );
 }
 
@@ -65,12 +25,12 @@ const styles = StyleSheet.create({
   },
 
   btn: {
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    ':hover': {
-      border: '4px solid #F0BF77',
-    },
-  },
+    paddingLeft: "10px",
+    paddingRight: "10px",
+    ":hover": {
+      border: "4px solid #F0BF77"
+    }
+  }
 });
 
 export default Login;
