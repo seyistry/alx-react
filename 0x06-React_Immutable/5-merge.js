fc01bb46f3194b170791b6574e8e444e5c03ba88
seyistry <seyistry@gmail.com>
2023-06-08 17:06:02 +0100

    refactor code

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -2,9 +2,13 @@ import { List, Map } from 'immutable';
 // import { List } from './node_modules/immutable/dist/immutable';
 
 export default function concatElements(page1, page2) {
-  return List(page1).concat(List(page2));
+  const list_1 = List(page1);
+  const list_2 = List(page2);
+  return list_1.concat(list_2);
 }
 
 export function mergeElements(page1, page2) {
-  return Map((page1)).merge(Map(page2));
+  obj_1 = Map(page1);
+  obj_2 = Map(page2);
+  return obj_1.merge(obj_2);
 }
