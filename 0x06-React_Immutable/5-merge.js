98a89cd600e9fa2a00d109fc105d738edb3c98e1
seyistry <seyistry@gmail.com>
2023-06-08 13:00:29 +0100

    test Map function

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,9 @@
 import { List, Map } from 'immutable';
 
 export default function concatElements(page1, page2) {
-  return List(page1);
+  return List(page1).merge(List(page2));
 }
 
 export function mergeElements(page1, page2) {
-  return Map(page1).merge(Map(page2));
+  return Map(page1);
 }
