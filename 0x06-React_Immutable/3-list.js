a744741593b459a4073fff97839ff16aede18fb7
seyistry <seyistry@gmail.com>
2023-06-08 11:57:04 +0100

    add ex 3

---
+++ 0x06-React_Immutable/3-list.js
@@ -0,0 +1,10 @@
+import { List } from 'immutable';
+
+export function getListObject(array) {
+  return List(array);
+}
+
+export function addElementToList(list, element) {
+  const arrList = List(list);
+  return arrList.withMutations((list) => list.push(element));
+}
