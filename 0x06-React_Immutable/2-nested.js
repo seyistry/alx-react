ee337867f942ee4f1942993484f0cfc14f5b4985
seyistry <seyistry@gmail.com>
2023-06-08 11:50:44 +0100

    add extra space at the end of file

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-	const value = fromJS(object)
-	return value.getIn(array)
+  const value = fromJS(object);
+  return value.getIn(array);
 }
