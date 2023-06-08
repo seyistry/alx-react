55b387effdf20d8c53e5a03ce950e9ff2f003d60
seyistry <seyistry@gmail.com>
2023-06-08 12:28:10 +0100

    test set function

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,4 @@
-// import { Map } from 'immutable';
-
-import { Map } from './node_modules/immutable/dist/immutable';
+import { Map } from 'immutable';
 
 export const map = Map({
   1: 'Liam',
@@ -11,7 +9,7 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = () => {
-  map.set('3', 'Benjamin');
-  map.set('5', 'Oliver');
-};
+export const map2 = map.set({
+  3: 'Benjamin',
+  5: 'Oliver',
+});
