13cad8b8b220bc8f7a2c437557a9f3d67b31cf40
seyistry <seyistry@gmail.com>
2023-06-08 13:29:24 +0100

    try List Mao object

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,4 +1,4 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 // import { List } from './node_modules/immutable/dist/immutable';
 
 export default function concatElements(page1, page2) {
@@ -6,5 +6,6 @@ export default function concatElements(page1, page2) {
 }
 
 export function mergeElements(page1, page2) {
-  return List(page1).merge(Map(page2));
+  const listObj = Map(page1).merge(Map(page2));
+  return List(listObj);
 }
