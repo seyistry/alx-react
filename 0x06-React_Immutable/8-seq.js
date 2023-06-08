a855f2ef6985da8c0fb9faa66da335ba99de6161
seyistry <seyistry@gmail.com>
2023-06-08 16:33:56 +0100

    test checker use cases

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,15 +2,15 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(obj) {
   Seq(obj).filter(function (key) {
-    if (key.score > 70) {
-      firstName = key.firstName;
-      lastName = key.lastName;
-      console.log(
-        `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName
-          .charAt(0)
-          .toUpperCase()}${lastName.slice(1)}`
-      );
-      return key;
-    }
+    // if (key.score > 70) {
+    //   firstName = key.firstName;
+    //   lastName = key.lastName;
+    //   console.log(
+    //     `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName
+    //       .charAt(0)
+    //       .toUpperCase()}${lastName.slice(1)}`
+    //   );
+    //   return key;
+    // }
   });
 }
