bb2ad1622208197197ad5ee9eab7d74f3233bd6a
seyistry <seyistry@gmail.com>
2023-06-08 14:16:30 +0100

    add ex 6

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,7 @@
+import { Map } from 'immutable';
+
+// import { Map } from './node_modules/immutable/dist/immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
+}
