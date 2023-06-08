fe0d7aa43f3b8cad5a54316e1069206f774daae7
seyistry <seyistry@gmail.com>
2023-06-08 16:22:53 +0100

    add ex 8

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,45 @@
+//#!/usr/bin/node
+import { Seq } from 'immutable';
+// const { Seq, Map } = require('immutable');
+// import { Seq } from './node_modules/immutable/dist/immutable';
+
+// const grades = {
+//   1: {
+//     score: 99,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   2: {
+//     score: 50,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   3: {
+//     score: 69,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   4: {
+//     score: 77,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+// };
+
+function printBestStudents(obj) {
+  Seq(obj).filter(function (key) {
+    if (key.score > 70) {
+      firstName = key.firstName;
+      lastName = key.lastName;
+      console.log(
+        `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName
+          .charAt(0)
+          .toUpperCase()}${lastName.slice(1)}`
+      );
+      return key;
+    }
+  });
+//   console.log(oddSquares.toObject());
+}
+
+// printBestStudents(grades);
