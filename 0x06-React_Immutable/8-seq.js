d7aa163e8a54c68e9c3fb599060a4a43649800d0
seyistry <seyistry@gmail.com>
2023-06-08 16:35:40 +0100

    add return statement

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,16 +1,16 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(obj) {
-  Seq(obj).filter(function (key) {
-    // if (key.score > 70) {
-    //   firstName = key.firstName;
-    //   lastName = key.lastName;
-    //   console.log(
-    //     `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName
-    //       .charAt(0)
-    //       .toUpperCase()}${lastName.slice(1)}`
-    //   );
-    //   return key;
-    // }
+  return Seq(obj).filter(function (key) {
+    if (key.score > 70) {
+      firstName = key.firstName;
+      lastName = key.lastName;
+      console.log(
+        `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName
+          .charAt(0)
+          .toUpperCase()}${lastName.slice(1)}`
+      );
+      return key;
+    }
   });
 }
