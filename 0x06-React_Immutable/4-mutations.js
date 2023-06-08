e84fdc1c442966ef80482ac7e363d1d9b6878bff
seyistry <seyistry@gmail.com>
2023-06-08 12:25:06 +0100

    add ex 4

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+// import { Map } from 'immutable';
+
+import { Map } from './node_modules/immutable/dist/immutable';
+
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+export const map2 = () => {
+  map.set('3', 'Benjamin');
+  map.set('5', 'Oliver');
+};
