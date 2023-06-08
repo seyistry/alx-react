c678d647b698376c05d5b791379f62378f145b92
seyistry <seyistry@gmail.com>
2023-06-08 13:08:34 +0100

    remove test for Map

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,10 @@
-import { List, Map } from 'immutable';
+import { List } from 'immutable';
+// import { List } from './node_modules/immutable/dist/immutable';
 
 export default function concatElements(page1, page2) {
-  return List(page1).merge(List(page2));
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements(page1, page2) {
-  return Map(page1).merge(Map(page1));
+  return List(page1).merge(Map(page2));
 }
