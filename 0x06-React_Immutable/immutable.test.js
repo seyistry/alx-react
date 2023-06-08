727859c976cd3d43c79d420302c7cd3e0ef3af1c
seyistry <seyistry@gmail.com>
2023-06-08 11:47:04 +0100

    add ex 2

--- 0x06-React_Immutable/immutable.test.js
+++ 0x06-React_Immutable/immutable.test.js
@@ -1,6 +1,7 @@
 // import {expect, jest, test} from '@jest';
 import fromJS from './0-fromjs';
-import Map from './1-map'
+import fromMap from './1-map';
+import { Map } from 'immutable';
 
 test('getImmutableObject fromJS function', () => {
   const test = fromJS({
@@ -9,11 +10,18 @@ test('getImmutableObject fromJS function', () => {
     wall: false,
     thing: -914767132,
   });
-  expect(test).toHaveReturned();
+  expect(test).toBe(
+    Map({
+      fear: true,
+      smell: -1033575916.9145899,
+      wall: false,
+      thing: -914767132,
+    })
+  );
 });
 
 test('getImmutableObject Map function', () => {
-  const test = Map({
+  const test = fromMap({
     fear: true,
     smell: -1033575916.9145899,
     wall: false,
@@ -21,4 +29,3 @@ test('getImmutableObject Map function', () => {
   });
   expect(test).toHaveReturned();
 });
-;
