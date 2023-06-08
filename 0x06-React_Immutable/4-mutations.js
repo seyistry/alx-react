8d32e53bbce64e1daec973fca06df42fac1ad1ae
seyistry <seyistry@gmail.com>
2023-06-08 14:04:13 +0100

    update ex

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,4 +1,5 @@
-import { Map } from 'immutable';
+// import { Map } from 'immutable';
+import { Map } from './node_modules/immutable/dist/immutable';
 
 export const map = Map({
   1: 'Liam',
@@ -9,7 +10,12 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map.set({
-  2: 'Benjamin',
-  4: 'Oliver',
-});
+// export const map2 = map.update({
+//   2: 'Benjamin',
+//   4: 'Oliver',
+// });
+
+export const map2 = map.map((value, key) => {
+  if (key == 3) value = "Benjamin"
+  if (key == 5) value = "Oliver"
+})
--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -2,11 +2,9 @@ import { List, Map } from 'immutable';
 // import { List } from './node_modules/immutable/dist/immutable';
 
 export default function concatElements(page1, page2) {
-  const listObj = List(page1).concat(List(page2));
-  return 'working';
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements(page1, page2) {
-  const listObj = Map(page1).merge(Map(page2));
-  return List(listObj);
+  return List(Map(page1)).merge(Map(page2));
 }
